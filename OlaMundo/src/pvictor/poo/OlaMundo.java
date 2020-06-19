a63e57e7106da938e28844257bec1925e80232ab
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-19 17:39:02 -0300

    Retirando a msg de teste

--- OlaMundo/src/pvictor/poo/OlaMundo.java
+++ OlaMundo/src/pvictor/poo/OlaMundo.java
@@ -3,6 +3,6 @@ package pvictor.poo;
 public class OlaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hello Word!");
-		System.out.println("Testando se esta atualizando no git");
+		
 	}
 }
