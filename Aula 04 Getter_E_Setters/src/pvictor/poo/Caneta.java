43b880957f74ac65118091e62df2dea402569b73
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-06-23 13:19:54 -0300

    Adicionando a aula 04 pelo sts

--- Aula 04 Getter_E_Setters/src/pvictor/poo/Caneta.java
+++ Aula 04 Getter_E_Setters/src/pvictor/poo/Caneta.java
@@ -1,57 +1,115 @@
 package pvictor.poo;
 
 public class Caneta {
-	private String modelo;
-	private float ponta;
-	private String cor;
-	private boolean tampada;
 
-	public Caneta(String modelo, float ponta, String cor) {
-		super();
-		this.modelo = modelo;
-		this.ponta = ponta;
-		this.cor = cor;
-	}
+    public String modelo;
+    private float ponta;
+    private String cor;
+    protected int carga; 
+    private boolean tampada;
 
-	public String getModelo() {
-		return modelo;
-	}
+    // clicando alt + insert para gerar o constructor 
+    // clicando ctrl 3 no eclipse e digitando gcuf vai criar o construtor
+    public Caneta(String modelo, String cor,float ponta) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.carga = carga;
+        this.ponta = ponta;
+      
+        
+    }
 
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
+	// clicando alt + insert para gerar os getters e setters no netbeans
+    // clicando ctrl 3 no eclipse e digitando ggas vai criar os metodos getters e setters
+    public String getModelo() {
+        return modelo;
+    }
 
-	public float getPonta() {
-		return ponta;
-	}
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
-	public void setPonta(float ponta) {
-		this.ponta = ponta;
-	}
+    public float getPonta() {
+        return ponta;
+    }
 
-	public String getCor() {
-		return cor;
-	}
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
 
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
+    public String getCor() {
+        return cor;
+    }
 
-	public boolean isTampada() {
-		return tampada;
-	}
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 
-	public void setTampada(boolean tampada) {
-		this.tampada = tampada;
-	}
+    public boolean isTampada() {
+        return tampada = true;// aqui da pra mudar se a caneta vai esta tampada ou destampada
+    }
 
-	public void status() {
-		System.out.println("SOBRE A CANETA: ");
-		System.out.println("Modelo: " + this.getModelo());
-		System.out.println("Ponta: "+ this.ponta);
-		System.out.println("Cor: " 	 +this.cor);
-		System.out.println("Tampada: "+ this.tampada);
-		
-	}
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+    public void status() {
+        System.out.println("SOBRE A CANETA: ");
+        System.out.println("Modelo: " + this.getModelo());//pode usar o this.modelo tambem
+        System.out.println("Ponta: " + this.getPonta()); // pode usar o this.ponta tambem
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada? " + this.isTampada());
+    }
 
+//    Aqui foram digitados todos os comandos.
+//    /* constructor simples sem parametros
+//    public Caneta(){
+//        this.tampar();
+//        this.cor = "Azul";        
+//    } */
+//    
+//    /*
+//        Constructor com parametros 
+//        m = modelo, c= cor, p = ponta
+//    */
+//    public Caneta(String m, String c, float p){
+//        this.modelo = m;
+//        this.cor = c;
+//        this.ponta = p;
+//        this.tampar();
+//    }
+//    
+//    public String getModelo() {
+//        return this.modelo;
+//    }
+//
+//    public void setModelo(String m) {
+//        this.modelo = m;
+//    }
+//
+//    public float getPonta() {
+//        return this.ponta;
+//    }
+//
+//    public void setPonta(float p) {
+//        this.ponta = p;
+//    }
+//
+//    public void tampar() {
+//        this.tampada = true;
+//    }
+//
+//    public void destampar() {
+//        this.tampada = false;
+//    }
+//
+//    public void status() {
+//        System.out.println("SOBRE A CANETA: ");
+//        System.out.println("Modelo: " + this.getModelo());//pode usar o this.modelo tambem
+//        System.out.println("Ponta: " + this.getPonta()); // pode usar o this.ponta tambem
+//        System.out.println("Cor: " + this.cor);
+//        System.out.println("Tampada? " + this.tampada);
+//    }
 }
+
+
